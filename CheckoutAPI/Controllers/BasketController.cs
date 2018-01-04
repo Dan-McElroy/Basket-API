0f5538ad19762aa3f4d97cf9d8af16ced8b1db58
Dan McElroy <daniel@mcelroys.net>
2018-01-04 09:59:22 +0000

    Added stubs for API endpoints.

---
+++ CheckoutAPI/Controllers/BasketController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckoutAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Basket")]
+    public class BasketController : Controller
+    {
+        [HttpPost]
+        public void AddItem()
+        {
+
+        }
+
+        [HttpPut]
+        public void EditItem()
+        {
+            
+        }
+
+        [HttpDelete]
+        public void DeleteItem()
+        {
+            
+        }
+
+        [HttpDelete("all")]
+        public void ClearBasket()
+        {
+            
+        }
+    }
+}
