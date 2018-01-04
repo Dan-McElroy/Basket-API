f79896a4a98928cf96b2c4f1fc5919f87361a530
Dan McElroy <daniel@mcelroys.net>
2018-01-04 11:37:11 +0000

    Added documentation to endpoint methods.

--- CheckoutAPI/Controllers/BasketController.cs
+++ CheckoutAPI/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace CheckoutAPI.Controllers
 {
@@ -11,32 +12,52 @@ namespace CheckoutAPI.Controllers
         /// Adds an item by its unique ID to the basket.
         /// </summary>
         /// <param name="itemId">The ID of the item to be added.</param>
-        /// <param name="quantity">An optional quantity for the item - the default is 1.</param>
+        /// <param name="quantity">
+        /// An optional quantity for the item - the default is 1.
+        /// </param>
         /// <remarks>
-        /// If the item already exists in the basket, the existing quantity is increased by the 
-        /// amount specified in the request, or 1 if no quantity is provided.
+        /// If the item already exists in the basket, the existing quantity is
+        /// increased by the amount specified in the request, or 1 if no 
+        /// quantity is provided.
         /// </remarks>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the quantity provided is below 1.
+        /// </exception>
         [HttpPost]
         public void AddItem(string itemId, int quantity = 1)
         {
-
+            if (quantity < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantity), 
+                    quantity, "Quantity must be greater than 0.");
+            }
         }
 
         /// <summary>
-        /// 
+        /// Changes the quantity of an item in the store.
         /// </summary>
-        /// <param name="itemId">The ID of the item to edit.</param>
+        /// <param name="itemId">
+        /// The ID of the item to edit the quantity of.
+        /// </param>
         /// <param name="quantity">The new quantity for the item.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if the item with the provided ID does not already exist in
+        /// the store.
+        /// </exception>
         [HttpPut]
-        public void EditItemQuantity()
+        public void EditItemQuantity(string itemId, int quantity)
         {
             
         }
 
         /// <summary>
-        /// 
+        /// Remove an item from the basket.
         /// </summary>
-        /// <param name="itemId"></param>
+        /// <param name="itemId">The ID of the item to remove.</param>
+        /// <remarks>
+        /// If the item does not exist in the basket, this method will return
+        /// sucessfully regardless.
+        /// </remarks>
         [HttpDelete]
         public void RemoveItem(string itemId)
         {
@@ -44,8 +65,12 @@ namespace CheckoutAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Clears the basket of items.
         /// </summary>
+        /// <remarks>
+        /// If the basket is already empty, this method will return succesfully
+        /// regardless.
+        /// </remarks>
         [HttpDelete("all")]
         public void ClearBasket()
         {
