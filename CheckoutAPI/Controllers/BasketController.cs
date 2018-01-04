7f464fea9e62f09f58cf83053abcfa4140863caa
Dan McElroy <daniel@mcelroys.net>
2018-01-04 11:06:06 +0000

    Changed EditItem endpoint to EditItemQuantity for clarity.

--- CheckoutAPI/Controllers/BasketController.cs
+++ CheckoutAPI/Controllers/BasketController.cs
@@ -7,24 +7,45 @@ namespace CheckoutAPI.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public class BasketController : Controller
     {
+        /// <summary>
+        /// Adds an item by its unique ID to the basket.
+        /// </summary>
+        /// <param name="itemId">The ID of the item to be added.</param>
+        /// <param name="quantity">An optional quantity for the item - the default is 1.</param>
+        /// <remarks>
+        /// If the item already exists in the basket, the existing quantity is increased by the 
+        /// amount specified in the request, or 1 if no quantity is provided.
+        /// </remarks>
         [HttpPost]
-        public void AddItem()
+        public void AddItem(string itemId, int quantity = 1)
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="itemId">The ID of the item to edit.</param>
+        /// <param name="quantity">The new quantity for the item.</param>
         [HttpPut]
-        public void EditItem()
+        public void EditItemQuantity()
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="itemId"></param>
         [HttpDelete]
-        public void DeleteItem()
+        public void DeleteItem(string itemId)
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [HttpDelete("all")]
         public void ClearBasket()
         {
