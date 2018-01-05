d18ee555d270e3f45ad998430cb715c9b41dcd90
Dan McElroy <daniel@mcelroys.net>
2018-01-05 15:31:31 +0000

    Added beginnings of BasketClient project.

---
+++ BasketSystem.Client/BasketClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+
+namespace BasketSystem.Client
+{
+    public class BasketClient : IDisposable
+    {
+        private HttpClient _client;
+
+        public BasketClient()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("TODO: Get from settings")
+            };
+        }
+
+        public void AddItem(string itemId, int quantity)
+        {
+
+        }
+
+        public void EditItemQuantity(string itemId, int quantity)
+        {
+
+        }
+
+        public void RemoveItem(string itemId)
+        {
+
+        }
+
+        public void ClearBasket()
+        {
+
+        }
+
+        #region IDisposable Methods
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        #endregion
+
+        private void SendRequest()
+        {
+
+        }
+    }
+}
