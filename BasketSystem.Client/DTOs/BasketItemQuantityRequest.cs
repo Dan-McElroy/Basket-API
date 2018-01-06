cfda5628b80d990ae593f92d2f7dd843036310a0
Dan McElroy <daniel@mcelroys.net>
2018-01-06 13:01:50 +0000

    Added request DTOs to the client.

---
+++ BasketSystem.Client/DTOs/BasketItemQuantityRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace BasketSystem.Client.DTOs
+{
+    /// <summary>
+    /// Represents the body of a request to the Basket API which alters the 
+    /// quantity of an item.
+    /// </summary>
+    public class BasketItemQuantityRequest : BasketItemRequest
+    {
+        /// <summary>
+        /// The quantity of the item to be updated.
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
---
+++ BasketSystem.Client/DTOs/BasketRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasketSystem.Client.DTOs
+{
+    /// <summary>
+    /// Represents the body of a request to the Basket API.
+    /// </summary>
+    public class BasketRequest
+    {
+        /// <summary>
+        /// The user token of the basket related to the
+        /// request.
+        /// </summary>
+        public Guid UserToken { get; set; }
+    }
+}
