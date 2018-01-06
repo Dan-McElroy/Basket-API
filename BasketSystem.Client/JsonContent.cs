26ef5140002e057633a9fbc60a544e901cc0acf3
Dan McElroy <daniel@mcelroys.net>
2018-01-06 13:57:46 +0000

    Added JsonContent class to easily produce JSON HTTP Content.

---
+++ BasketSystem.Client/JsonContent.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace BasketSystem.Client
+{
+    /// <summary>
+    /// Represents request content in JSON.
+    /// </summary>
+    internal class JsonContent : StringContent
+    {
+        public JsonContent(object content)
+        : base(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json")
+        {
+        }
+    }
+}
