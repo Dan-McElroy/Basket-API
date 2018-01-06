c6efae34f8342a842832ce2aea0eea1ac0b76086
Dan McElroy <daniel@mcelroys.net>
2018-01-06 14:51:04 +0000

    Altered route of basket creation endpoint for readability.

--- BasketSystem.API/Controllers/BasketController.cs
+++ BasketSystem.API/Controllers/BasketController.cs
@@ -27,7 +27,7 @@ namespace BasketAPI.Controllers
         /// Creates a new <see cref="Basket"/> for the User.
         /// </summary>
         /// <returns>The token required to access this basket.</returns>
-        [HttpPost("new-basket")]
+        [HttpPost("new")]
         public Guid CreateBasket()
             => Repository.CreateBasket();
 
