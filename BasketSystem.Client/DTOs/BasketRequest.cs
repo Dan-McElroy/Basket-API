f3935bc368e80f55ac03dd5ad2862bc2a114aefe
Dan McElroy <daniel@mcelroys.net>
2018-01-06 13:57:31 +0000

    Condensed DTOs into one.

--- BasketSystem.Client/DTOs/BasketRequest.cs
+++ BasketSystem.Client/DTOs/BasketRequest.cs
@@ -3,14 +3,31 @@
 namespace BasketSystem.Client.DTOs
 {
     /// <summary>
-    /// Represents the body of a request to the Basket API.
+    /// Represents the body of a request to the Basket API which alters the 
+    /// quantity of an item.
     /// </summary>
     public class BasketRequest
     {
         /// <summary>
-        /// The user token of the basket related to the
-        /// request.
+        /// The unique token to access the user's basket.
         /// </summary>
-        public Guid UserToken { get; set; }
+        public Guid Token { get; set; }
+
+        /// <summary>
+        /// The ID of the item to be altered in the basket.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The quantity of the item to be updated.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        public BasketRequest(Guid token, string id, int quantity)
+        {
+            Token = token;
+            Id = id;
+            Quantity = quantity;
+        }
     }
 }
