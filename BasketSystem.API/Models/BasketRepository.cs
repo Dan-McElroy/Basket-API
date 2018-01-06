3eec55e4134c45a380f380fb87d4ab7d393ad7d8
Dan McElroy <daniel@mcelroys.net>
2018-01-06 12:19:32 +0000

    Created BasketRepository to hold baskets for all users.

---
+++ BasketSystem.API/Models/BasketRepository.cs
@@ -0,0 +1,29 @@
+﻿using BasketSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketSystem.API.Models
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private Dictionary<Guid, IBasket> Baskets { get; }
+
+        public BasketRepository()
+        {
+            Baskets = new Dictionary<Guid, IBasket>();
+        }
+
+        public Guid CreateBasket()
+        {
+            var userToken = Guid.NewGuid();
+            var basket = new Basket();
+            Baskets.Add(userToken, basket);
+            return userToken;
+        }
+
+        public IBasket FindBasket(Guid userToken)
+            => Baskets.GetValueOrDefault(userToken);
+    }
+}
---
+++ BasketSystem.API/Models/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using BasketSystem.Models;
+using System;
+
+namespace BasketSystem.API.Models
+{
+    public interface IBasketRepository
+    {
+        Guid CreateBasket();
+
+        IBasket FindBasket(Guid userToken);
+    }
+}
